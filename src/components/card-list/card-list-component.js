2394c81432585504d47275d064933c916e4ecfef
carycheng <carycheng77@gmail.com>
2021-08-08 11:09:05 -0700

    added card component

--- src/components/card-list/card-list-component.js
+++ src/components/card-list/card-list-component.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { Card } from '../card/card-component';
+
+import './card-list-style.css';
+
 export const CardList = props => {
     return (
-        <div>Hello</div>
+        <div className="card-list">
+            {props.monsters.map(monster => {
+                <Card key={monster.id} monster={monster} />
+            })};    
+        </div>
     );
 };
